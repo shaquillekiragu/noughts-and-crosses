399dfcac37427eff1ad88e21434483ada374f6b5
Shaquille Kiragu <skiragu77@gmail.com>
2026-01-01 22:33:52 +0000

    ui wip 5

--- app/components/grid-square/component.vue
+++ app/components/grid-square/component.vue
@@ -11,13 +11,13 @@
 			v-if="grid_square.content_index === 1"
 			class="w-2/3 h-2/3 bg-black flex justify-center items-center rounded-full"
 		>
-			<div class="w-3/4 h-3/4 rounded-full bg-slate-100 z-1"></div>
+			<div class="w-2/3 h-2/3 rounded-full bg-slate-100 z-1"></div>
 		</div>
 
 		<Icon
 			v-if="grid_square.content_index === 2"
-			name="i-fluent-emoji-high-contrast:cross-mark"
-			class="w-2/3 h-2/3"
+			name="fluent-emoji-high-contrast:cross-mark"
+			class="!w-2/3 !h-2/3"
 		/>
 	</article>
 </template>
