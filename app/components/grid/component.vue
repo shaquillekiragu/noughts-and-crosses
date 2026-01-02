16427a6377377b9ac3fc0ec5ae4be8bcf6de0f66
Shaquille Kiragu <skiragu77@gmail.com>
2026-01-02 00:36:40 +0000

    logic wip 3

--- app/components/grid/component.vue
+++ app/components/grid/component.vue
@@ -3,7 +3,7 @@
 		class="grid grid-cols-3 w-3/4 sm:w-2/3 md:w-3/5 lg:w-1/2 xl:w-2/5 2xl:w-1/3 aspect-square divide-x-5 divide-y-5 sm:divide-x-6 sm:divide-y-6 xl:divide-x-8 xl:divide-y-8"
 	>
 		<GridSquareComponent
-			v-for="(grid_square, index) in grids"
+			v-for="(grid_square, index) in grid"
 			:key="index"
 			:grid_square="grid_square"
 			:index="index"
@@ -16,8 +16,8 @@
 import type { PropType } from "vue";
 import type GridSquare from "~~/types/grid-square.ts";
 
-const { grids } = defineProps({
-	grids: Array as PropType<GridSquare[]>,
+const { grid } = defineProps({
+	grid: Array as PropType<GridSquare[]>,
 });
 
 const emits = defineEmits<{
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,55 +2,44 @@
 	<main class="w-screen min-h-screen flex flex-col items-center my-10">
 		<h1 class="text-center text-3xl font-bold mb-15">Hello World!</h1>
 		<GridComponent
-			:grids="grids"
-			@on-click-emit-two="handleSquareClick"
+			:grid="grid"
+			@on-click-emit-two="handlePlayerMove"
 			class=""
 		/>
 	</main>
 </template>
 
 <script setup lang="ts">
+import { nextTick } from "vue";
+import checkForWin from "~/composables/checkForWin";
 import type GridSquare from "~~/types/grid-square.ts";
+import type CheckForWinReturnObject from "~~/types/check-for-win-return-object";
 
-const grids = ref<GridSquare[]>([
-	{
-		content_index: 0,
-	},
-	{
-		content_index: 0,
-	},
-	{
-		content_index: 0,
-	},
-	{
-		content_index: 0,
-	},
-	{
-		content_index: 1,
-	},
-	{
-		content_index: 2,
-	},
-	{
-		content_index: 0,
-	},
-	{
-		content_index: 0,
-	},
-	{
-		content_index: 0,
-	},
-]);
+const grid = ref<GridSquare[]>([]);
 
-// for (let i = 0; i < 9; i++) {
-// 	const grid_square: GridSquare = {
-// 		content_index: 0,
-// 	};
+for (let i = 0; i < 9; i++) {
+	grid.value.push({
+		content_index: 0,
+	});
+}
 
-// 	grids.push(grid_square);
-// }
+console.log(grid.value, " < initial grid");
 
-function handleSquareClick(index: number, new_content_index: 1 | 2) {
-	grids.value[index].content_index = new_content_index;
+async function handlePlayerMove(index: number, new_content_index: 1 | 2) {
+	grid.value[index].content_index = new_content_index;
+
+	await nextTick();
+
+	const win_info: CheckForWinReturnObject = checkForWin(grid.value);
+
+	if (win_info.meets_win_condition === true) {
+		setTimeout(() => {
+			console.log("win");
+			for (const square of grid.value) {
+				square.content_index = 0;
+			}
+			alert("You win! Click 'Ok' to play again.");
+		}, 300);
+	}
 }
 </script>
