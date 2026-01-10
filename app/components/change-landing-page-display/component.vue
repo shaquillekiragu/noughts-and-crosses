bba8a080c422748063baff4115307d2068778e74
Shaquille Kiragu <skiragu77@gmail.com>
2026-01-10 23:21:29 +0000

    logic wip 7

--- app/components/change-landing-page-display/component.vue
+++ app/components/change-landing-page-display/component.vue
@@ -1,20 +1,20 @@
 <template>
-	<NuxtLink
-		:to="link_url"
+	<button
 		class="w-10 sm:w-12 aspect-square flex justify-center items-center rounded-full bg-slate-300"
 	>
 		<Icon
+			v-else
 			name="material-symbols:chevron-left-rounded"
 			class="!w-2/3 !h-2/3"
 		/>
-	</NuxtLink>
+	</button>
 </template>
 
-<script setup lang="ts">
-const { link_url } = defineProps({
-	link_url: {
+<!-- <script setup lang="ts">
+const { styles } = defineProps({
+	styles: {
 		type: String,
 		required: true,
 	},
 });
-</script>
+</script> -->
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,42 @@
 			Noughts and Crosses
 		</h1>
 
-		<section class="w-full flex flex-col items-center">
+		<section
+			v-if="landing_page_display === 0"
+			class="w-full flex flex-col items-center"
+		>
+			<h2 class="text-center text-lg sm:text-xl font-bold mb-10">
+				Which game mode do you want to play?
+			</h2>
+
+			<div
+				class="flex flex-col items-center gap-8 *:w-60 *:h-15 *:flex *:justify-center *:items-center *:rounded-lg *:text-xl *:text-white *:hover:cursor-pointer"
+			>
+				<button
+					class="bg-blue-800"
+					@click="handleLandingPageDisplayChange(1)"
+				>
+					Player vs Player
+				</button>
+
+				<button
+					class="bg-green-800"
+					@click="handleLandingPageDisplayChange(2)"
+				>
+					Player vs Computer
+				</button>
+			</div>
+		</section>
+
+		<section
+			v-else-if="landing_page_display === 1"
+			class="w-full flex flex-col items-center"
+		>
+			<ChangeLandingPageDisplayComponent
+				class="absolute left-[10vw] top-15"
+				@click="handleLandingPageDisplayChange(0)"
+			/>
+
 			<h2 class="text-center text-lg sm:text-xl font-bold mb-10">
 				Player 1: Choose your side:
 			</h2>
@@ -32,6 +67,17 @@
 			</div>
 		</section>
 
+		<section v-else class="w-full flex flex-col items-center">
+			<ChangeLandingPageDisplayComponent
+				class="absolute left-[10vw] top-15"
+				@click="handleLandingPageDisplayChange(0)"
+			/>
+
+			<h2 class="text-center text-lg sm:text-xl font-bold mb-10">
+				Player, choose your side:
+			</h2>
+		</section>
+
 		<FooterComponent />
 	</main>
 </template>
@@ -40,6 +86,11 @@
 import useGameState from "~/composables/useGameState";
 
 const { player_symbol_map } = useGameState();
+const landing_page_display = ref<0 | 1 | 2>(0);
+
+function handleLandingPageDisplayChange(index: 0 | 1 | 2) {
+	landing_page_display.value = index;
+}
 
 function handleChooseSymbolClick(symbol_index: 1 | 2) {
 	player_symbol_map.value.player_one = symbol_index;
@@ -47,4 +98,8 @@ function handleChooseSymbolClick(symbol_index: 1 | 2) {
 	player_symbol_map.value.player_two =
 		player_symbol_map.value.player_one === 1 ? 2 : 1;
 }
+
+onMounted(() => {
+	landing_page_display.value = 0;
+});
 </script>
